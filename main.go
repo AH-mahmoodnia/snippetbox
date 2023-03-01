e5f7a29c516ccb0ab3b8f94b3b317e15ad304a70
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-03-01 10:49:10 +0330

    create a simple file which contain three handler for home for creating snippet and viewing the created one this is gonna be removed in the future and just for starting something and practicing git.

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello This is the home"))
+}
+func view(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display the snippet"))
+}
+func create(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+	w.Write([]byte("Create a snippet"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", view)
+	mux.HandleFunc("/snippet/create", create)
+	fmt.Println("Server Starting on port 4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
