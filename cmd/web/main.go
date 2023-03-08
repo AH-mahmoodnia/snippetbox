62c5c47c8cdec38ca0e661cba55b71bb3ebe94a7
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-03-08 19:01:24 +0330

    create a new file routes.go to handle routing outside of main and change the app to contain the  config struct as a filed so we can access that in routes.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,35 +15,26 @@ type config struct {
 type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	cfg      config
 }
 
 func main() {
-	var cfg config
-	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network adderss")
-	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
-	flag.Parse()
-
 	app := &application{
 		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile),
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	flag.StringVar(&app.cfg.addr, "addr", ":4000", "HTTP network adderss")
+	flag.StringVar(&app.cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
 
 	srv := &http.Server{
-		Addr:     cfg.addr,
-		Handler:  mux,
+		Addr:     app.cfg.addr,
+		Handler:  app.routes(),
 		ErrorLog: app.errorLog,
 	}
 
-	app.infoLog.Printf("Starting server on %s", cfg.addr)
+	app.infoLog.Printf("Starting server on %s", app.cfg.addr)
 	err := srv.ListenAndServe()
 	app.errorLog.Fatal(err)
 }
