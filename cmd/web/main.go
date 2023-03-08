b16c4fd82042ea17e42ab39f18262aa546493ef0
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-03-08 20:48:54 +0330

    specifying the attributes to use lib/pq driver and postgresql database for handling the data part.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type config struct {
 	addr      string
 	staticDir string
+	dsn       string
 }
 
 type application struct {
@@ -26,7 +30,14 @@ func main() {
 
 	flag.StringVar(&app.cfg.addr, "addr", ":4000", "HTTP network adderss")
 	flag.StringVar(&app.cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.StringVar(&app.cfg.dsn, "dsn", "postgres://web:123@localhost/snippetbox?sslmode=disable", "Postgresql data source name")
 	flag.Parse()
+
+	db, err := app.openDB()
+	if err != nil {
+		app.errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	srv := &http.Server{
 		Addr:     app.cfg.addr,
@@ -35,6 +46,17 @@ func main() {
 	}
 
 	app.infoLog.Printf("Starting server on %s", app.cfg.addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.errorLog.Fatal(err)
+}
+
+func (app *application) openDB() (*sql.DB, error) {
+	db, err := sql.Open("postgres", app.cfg.dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AH-mahmoodnia/snippetbox
 
 go 1.19
+
+require github.com/lib/pq v1.10.7
