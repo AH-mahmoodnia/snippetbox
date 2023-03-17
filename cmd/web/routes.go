89ed7650f1536906109e7f08ba32cbe0c9663317
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-03-17 17:34:05 +0330

    add new recoverPanic middleware method on applicaion so panics happend in any request's goroutin handled well so the internal server error will show to user instead of nothing.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,6 +12,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return app.logRequest(secureHeader(mux))
+	return app.recoverPanic(app.logRequest(secureHeader(mux)))
 
 }
