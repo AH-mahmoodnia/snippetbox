0c30c5767683c344ccb20bb3f96a1cc4fce0d7c6
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-04-21 16:50:03 +0330

    using the mock model test the snippetView handler.

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,11 +1,22 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
 		t.Errorf("got: %v; want: %v", actual, expected)
+	}
+}
+
+func StringContains(t *testing.T, actual, expectdSubstring string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expectdSubstring) {
+		t.Errorf("got: %q; expected to contain: %q", actual, expectdSubstring)
 	}
 }
